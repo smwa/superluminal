77c08ec00a572d06d78a305a8781086f44df4989
Michael Smith <michael.smith.ok@gmail.com>
2021-06-19 15:23:51 -0500

    Add photon support

--- superluminal/server/Body.py
+++ superluminal/server/Body.py
@@ -1,7 +1,10 @@
 from Vector3 import Vector3
 from Vector4 import Vector4
+from speed_of_light import SPEED_OF_LIGHT
 
 class Body(object):
+    PHOTON_RADIUS = 0.000000000001
+
     def __init__(self):
         self.in_use = True # Used for object pooling
         self.mass = 0.0 # Kg
@@ -19,3 +22,11 @@ class Body(object):
 
     def __repr__(self) -> str:
         return "In Use: {}\nMass: {}\nRadius: {}\nPosition: {}\nVelocity: {}\nRotation: {}".format(self.in_use, self.mass, self.radius, self.position, self.velocity, self.rotation)
+
+    def set_photon_velocity_by_direction(self, direction):
+        self.velocity = direction.normalize() * SPEED_OF_LIGHT
+    
+    def set_photon_mass_by_electromagnetic_frequency(self, frequency_in_hertz):
+        electron_volts = frequency_in_hertz * 4.1356655385381 * 10**-15
+        energy = electron_volts * 1.602177 * 10**-19
+        self.mass = energy / SPEED_OF_LIGHT**2
--- superluminal/server/index.py
+++ superluminal/server/index.py
@@ -1,4 +1,6 @@
+from Vector3 import Vector3
 from Engine import Engine
+from Body import Body
 
 e = Engine()
 a = e.get_new_body()
@@ -7,6 +9,12 @@ a.position.x = 10
 b = e.get_new_body()
 b.mass = 10
 b.velocity.z = 3
+c = e.get_new_body()
+c.radius = Body.PHOTON_RADIUS
+direction = Vector3()
+direction.x = 1.0
+c.set_photon_velocity_by_direction(direction)
+c.set_photon_mass_by_electromagnetic_frequency(500*10**12) # Visible light
 while True:
     e.tick()
     print(b.position)
